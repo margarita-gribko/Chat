23cece93d883d958af80d6240849988b7d17dd66
margo <margarita.gribko@yandex.ru>
2020-09-22 11:59:45 +0300

    add my code

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Margo on 8/3/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBuble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightEmageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBuble.layer.cornerRadius = messageBuble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
